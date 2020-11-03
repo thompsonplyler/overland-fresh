592457e0156908e7b3dcbab37c9aac49bdce1d8c
Thompson Plyler <nealok.g@gmail.com>
2020-11-03 15:57:20 -0500

    Removed line from post-event page.

--- src/components/PostEvent.jsx
+++ src/components/PostEvent.jsx
@@ -12,7 +12,7 @@ function PostEvent() {
     return(
     <div className="container">
           <img className="img-fresh-logo" src={freshLogo}/>
-          <h2 style={{color: "white", fontFamily: "PTSerif"}}>Worldwide Meeting 2020</h2> 
+          {/* <h2 style={{color: "white", fontFamily: "PTSerif"}}>Worldwide Meeting 2020</h2>  */}
           <h2 className="registration-heading-1">under one sky</h2>
           <p className="para1">Thank you for coming!</p>
     <ClientPendingBanner subject="post-event"/>
