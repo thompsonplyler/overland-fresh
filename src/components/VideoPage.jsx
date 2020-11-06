1185e941915a0fe43a8f0d71587086cfded3e6aa
Thompson Plyler <nealok.g@gmail.com>
2020-11-06 15:45:01 -0500

    Cleaner navigation for client. Chat and authentication still unwired.

--- src/components/VideoPage.jsx
+++ src/components/VideoPage.jsx
@@ -1,48 +1,39 @@
 import React from 'react'
 import {Fragment, useState, useEffect} from 'react'
-
 import videojs from 'video.js'
 import awsvideoconfig from '../aws-video-exports'
 import 'video.js/dist/video-js.css'
 import '../App.css';
-import '../Video.css';
 import freshLogo from '../assets/images/frshlogo.svg'
 import videoFile from "../assets/videos/160825_05_Sunflowers3_1080p.mp4"
 import ClientPendingBanner from '../components/ClientPendingBanner'
 import FakeChat from './FakeChat'
-import { registerIVSTech } from 'amazon-ivs-player';
-// import { setupForm, getFormStream } from '../components/common/form-control'
-
-window.VIDEOJS_NO_DYNAMIC_STYLE = true
-
+  
 const videoJsOptions = {
-  autoplay: false,
-  controls: true,
-  loop: true,
-  poster: "https://i.imgur.com/Aaog0bm.png",
-  sources: [{
-    src: "https://i.imgur.com/8kDpUiB.mp4",
-    // src: awsvideoconfig.awsOutputLiveLL,
-    poster: "https://i.imgur.com/Aaog0bm.png"
-  }]
-}
-registerIVSTech(videojs, videoJsOptions);
+    autoplay: true,
+    controls: true,
+    loop: true,
+    poster: "https://i.imgur.com/Aaog0bm.png",
+    sources: [{
+      src: "https://i.imgur.com/8kDpUiB.mp4",
+      // src: awsvideoconfig.awsOutputLiveLL,
+      poster: "https://i.imgur.com/Aaog0bm.png"
+    }]
+  }
   
   function VideoPage() {
 
     class VideoPlayer extends React.Component {
       constructor(props) {
         super(props);
+       
+       
         }
     
       // console.log(props)
     
         componentDidMount(props) {
-        //   const player = videojs('videojs-player', {
-        //     techOrder: ["AmazonIVS"]
-        // });
           this.player = videojs(this.videoNode, this.props)
-
         }
       
         componentWillUnmount() {
@@ -55,22 +46,12 @@ registerIVSTech(videojs, videoJsOptions);
           let {windowHeight, windowWidth} = this.props
           let newWidth = windowWidth *.60
           let newHeight = newWidth * .5625
-          console.log("props are:", this.props)
-
-          let PLAYBACK_URL = '';
-          registerIVSTech(videojs)
-          
-          // let newPlayer = videojs('amazon-ivs-videojs',{
-          //   techOrder:['AmazonIVS']
-          // }, ()=>{
-          //   console.log("Player is ready to use!")
-          //   newPlayer.src(PLAYBACK_URL);
-          // })
-          // console.log(this)
-          return (
-            <div className="">
+          console.log("rendering:", this)
     
-              <div data-vjs-player style={{
+          return (
+            <div className="video-player">
+    
+              <div  data-vjs-player style={{
                 width: newWidth,
                 height: newHeight
               }}>
@@ -89,17 +70,24 @@ registerIVSTech(videojs, videoJsOptions);
     window.addEventListener('resize',()=>{
       setHeight(window.innerHeight)
       setWidth(window.innerWidth)
+      console.log(stateWidth, stateHeight)
     })
 
     return (
-    <div className="grid-container">
-      <h2 className="registration-heading-grid">under one sky</h2>
-      <div className="src-input"></div>
-        <div className="video-row">
-          <VideoPlayer windowHeight={stateHeight} windowWidth={stateWidth}{ ...videoJsOptions }/>
-          </div>
-        <img className="grid-heading" style={{width: "6vw"}} src={freshLogo}/>
-    </div>
+              <div className="grid-container">
+            {/* <img className="img-fresh-logo" src={freshLogo}/> */}
+            
+            <h2 className="registration-heading-grid">under one sky</h2>
+
+            <div className="video-row" style={{padding: "3vh"}}>
+              {/* <div className="chat-area">Test</div> */}
+              <VideoPlayer windowHeight={stateHeight} windowWidth={stateWidth}{ ...videoJsOptions }/>
+              <FakeChat windowHeight={stateHeight} windowWidth={stateWidth}/>
+            
+            </div>
+            <img className="grid-heading" style={{width: "6vw"}} src={freshLogo}/>
+            <ClientPendingBanner subject="event"/>
+        </div>
 
 
     );
