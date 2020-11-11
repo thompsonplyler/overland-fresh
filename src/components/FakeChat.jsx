30fb82ce98b44508c2480a920066c878b8d201af
Thompson Plyler <nealok.g@gmail.com>
2020-11-11 11:41:08 -0500

    Beginning chat implementation.

--- src/components/FakeChat.jsx
+++ src/components/FakeChat.jsx
@@ -1,28 +1,12 @@
+import React, { Component, Fragment } from 'react';
 import '../App.css';
 
-
-function FakeChat(props) {
-    // const randomphrases = ['We the people','in order to form','a more perfect union','establish justice','insure domestic tranquility','provide for the common defence, promote the general welfare','and secure the blessings of liberty','to ourselves','and our posterity','do ordain and establish this constitution','of the United States of America'];
-    // console.log("I run.")
-    // let c = 0;
-    // let t;
-    // let timer_is_on = 0;
-    
-    // timedCount();
-    
-    // function timedCount()
-    // {
-    //  t = setTimeout( function(){ 
-    //      timedCount() 
-    //     }, 1500 );
-
-        
-    //     //  $('.putmehere').html( randomphrases[random]);
-        
-    // }
-    
-    // let random = Math.floor(Math.random() * (11 - 0 + 1)) + 0;
-
+export default class extends Component {
+    state = {
+      loaded: true
+    }
+render(){
+ 
     return(
         
         <div className="chat-area">
@@ -30,6 +14,7 @@ function FakeChat(props) {
             <p>[chat box location]</p>
         </div>
     )
+}
 }
 
 export default FakeChat;
