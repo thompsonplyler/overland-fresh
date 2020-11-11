5ff470a5c1a30de7fe140261b00f038fbe23130b
Thompson Plyler <nealok.g@gmail.com>
2020-11-11 04:04:23 -0500

    Added: * auth and email confirmation sending through Rails server * more comprehensive access/refusal through state.

--- src/components/AddToCalendar.jsx
+++ src/components/AddToCalendar.jsx
@@ -27,7 +27,7 @@ componentDidMount() {
 this.state.loaded?<div title="Add to Calendar" className="addeventatc">
   
     Add to Calendar
-    <span className="start">11/17/2020 07:30 AM</span>
+    <span className="start">11/17/2020 08:00 AM</span>
     <span className="end">11/17/2020 10:00 AM</span>
     <span className="timezone">America/New_York
 </span>
