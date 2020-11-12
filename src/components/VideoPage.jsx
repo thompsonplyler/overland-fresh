2f8acd1832bd602205aa03bc8563d5ec4e2e88e4
Thompson Plyler <nealok.g@gmail.com>
2020-11-12 13:41:10 -0500

    Changed video to static file for client usage.

--- src/components/VideoPage.jsx
+++ src/components/VideoPage.jsx
@@ -17,8 +17,8 @@ const videoJsOptions = {
     responsive: true,
     poster: "https://i.imgur.com/Aaog0bm.png",
     sources: [{
-      // src: "https://i.imgur.com/8kDpUiB.mp4",
-      src: awsvideoconfig.awsOutputLiveLL,
+      src: "https://i.imgur.com/8kDpUiB.mp4",
+      // src: awsvideoconfig.awsOutputLiveLL,
       poster: "https://i.imgur.com/Aaog0bm.png"
     }]
   }
