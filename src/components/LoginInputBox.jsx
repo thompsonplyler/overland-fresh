6b3a8887d3f22b07953e3f0b57b25274c75977cb
Thompson Plyler <nealok.g@gmail.com>
2020-11-13 17:08:08 -0500

    Added some stuff about IE polyfill. Who knows. I'm firing in the dark without tracers here. I hate IE.

--- src/components/LoginInputBox.jsx
+++ src/components/LoginInputBox.jsx
@@ -1,3 +1,19 @@
+// import 'core-js/array/includes';
+import 'core-js/es/symbol';
+import 'core-js/es/object';
+import 'core-js/es/function';
+import 'core-js/es/parse-int';
+import 'core-js/es/parse-float';
+import 'core-js/es/number';
+import 'core-js/es/math';
+import 'core-js/es/string';
+import 'core-js/es/date';
+import 'core-js/es/array';
+import 'core-js/es/regexp';
+import 'core-js/es/map';
+import 'core-js/es/weak-map';
+import 'core-js/es/set';
+import 'core-js/es/array';
 import React from 'react'
 import {Component} from 'react'
 import axios from 'axios'
@@ -22,16 +38,6 @@ class LoginInputBox extends Component{
           errors: ''
          };
       }
-
-
-
-      
-    
-    inputDetermine = () => {
-
-  
-        return
-    }
 
     handleChange = (event) => {
         const {name, value} = event.target
