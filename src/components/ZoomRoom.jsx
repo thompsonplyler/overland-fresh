77c22f3f7dcd213a06fed6488c4ff910b822e525
thompsonplyler <nealok.g@gmail.com>
2020-12-10 10:17:20 -0500

    Changed test site address to new IVS URI.

---
+++ src/components/ZoomRoom.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../App.css';
+import { ZoomMtg } from '@zoomus/websdk'
+
+ZoomMtg.setZoomJSLib('node_modules/@zoomus/websdk/dist/lib', '/av');
+ZoomMtg.preLoadWasm();
+ZoomMtg.prepareJssdk
+
+
+function ZoomRoom() {
+    return (
+        <div>
+            Test
+        </div>
+    )
+}
+
+export default ZoomRoom;
