6447223ac424ce631d21c63af9c25e23856031a0
thompsonplyler <nealok.g@gmail.com>
2020-12-10 11:01:06 -0500

    Changed to use as staging site.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import {Component} from 'react'
+import { Component } from 'react'
 
 import {
   BrowserRouter as Router,
@@ -7,13 +7,13 @@ import {
   Route,
   withRouter
 } from "react-router-dom";
-import {createBrowserHistory} from 'history'
+import { createBrowserHistory } from 'history'
 import VideoPage from "./components/VideoPage"
 import PreEvent from "./components/PreEvent"
 import PostEvent from "./components/PostEvent"
 import Login from "./components/Login"
 import LoginFailed from "./components/LoginFailed"
-import {LOGIN_URL, EVENT_URL, CONFIRMATION_URL, POST_EVENT_URL, LOGIN_FAILED_URL} from './urls'
+import { LOGIN_URL, EVENT_URL, CONFIRMATION_URL, POST_EVENT_URL, LOGIN_FAILED_URL } from './urls'
 import ReactGA from 'react-ga';
 
 // const trackingId = ""
@@ -25,10 +25,10 @@ import ReactGA from 'react-ga';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       isLoggedIn: false,
       user: {}
-     };
+    };
   }
 
   componentDidMount() {
@@ -36,9 +36,9 @@ class App extends Component {
     ReactGA.pageview(window.location.pathname);
     let localUser = localStorage.getItem('user');
     if (localUser) {
-      const user = JSON.parse(localUser); 
+      const user = JSON.parse(localUser);
       this.setState({
-        isLoggedIn: true, 
+        isLoggedIn: true,
         user
       });
       // console.log('USER PRESENT', user);
@@ -55,12 +55,12 @@ class App extends Component {
 
   handleLogout = () => {
     this.setState({
-    isLoggedIn: true,
-    user: {}
+      isLoggedIn: true,
+      user: {}
     })
   }
 
-  render(){
+  render() {
     const history = createBrowserHistory();
 
     // Initialize google analytics page view tracking
@@ -69,10 +69,10 @@ class App extends Component {
       ReactGA.set({ page: location.pathname }); // Update the user's current page
       ReactGA.pageview(location.pathname); // Record a pageview for the given page
     });
-    
+
     return (
-      
-      
+
+
       <Router>
         <Switch>
           <Route
@@ -88,7 +88,7 @@ class App extends Component {
               />
             )}
           />
-          
+
           <Route
             exact
             path={CONFIRMATION_URL}
@@ -98,13 +98,13 @@ class App extends Component {
                 {...props}
                 isLoggedIn={this.state.isLoggedIn}
                 user={this.state.user}
-                
-                
+
+
               />
             )}
           />
 
-        <Route
+          <Route
             exact
             path={EVENT_URL}
             params={this.props.match}
@@ -115,16 +115,16 @@ class App extends Component {
                 user={this.state.user}
                 handleLogin={this.handleLogin}
               />
-              )}
-              />
-    
+            )}
+          />
+
 
           <Route
             exact
             path="/"
             history={history}
             render={(props) => (
-              <Login
+              <VideoPage
                 {...props}
                 isLoggedIn={this.state.isLoggedIn}
                 user={this.state.user}
@@ -145,7 +145,7 @@ class App extends Component {
             )}
           />
 
-<Route
+          <Route
             exact
             path={LOGIN_FAILED_URL}
             history={history}
@@ -157,11 +157,11 @@ class App extends Component {
               />
             )}
           />
-          
+
         </Switch>
       </Router>
     );
-    }
   }
+}
 
 export default withRouter(App);
--- src/components/VideoPage.jsx
+++ src/components/VideoPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import videojs from 'video.js'
 import awsvideoconfig from '../aws-video-exports'
 import 'video.js/dist/video-js.css'
@@ -7,97 +7,97 @@ import '../App.css';
 import freshLogo from '../assets/images/frshlogo.svg'
 import ClientPendingBanner from '../components/ClientPendingBanner'
 import FakeChat from './FakeChat'
-import {checkUserCreds} from '../components/checkUserCreds'
+import { checkUserCreds } from '../components/checkUserCreds'
 
-  
+
 const videoJsOptions = {
-    autoplay: true,
-    controls: true,
-    loop: true,
-    responsive: true,
-    poster: "https://i.imgur.com/Aaog0bm.png",
-    sources: [{
-      // src: "https://i.imgur.com/8kDpUiB.mp4",
-      src: awsvideoconfig.awsOutputLiveLL,
-      poster: "https://i.imgur.com/Aaog0bm.png"
-    }]
-  }
-  
+  autoplay: true,
+  controls: true,
+  loop: true,
+  responsive: true,
+  poster: "https://i.imgur.com/Aaog0bm.png",
+  sources: [{
+    src: "https://i.imgur.com/8kDpUiB.mp4",
+    // src: awsvideoconfig.awsOutputLiveLL,
+    poster: "https://i.imgur.com/Aaog0bm.png"
+  }]
+}
 
 
 
 
 
-  function VideoPage(props) {
 
-    // useEffect(() => {
-    //   const user = checkUserCreds(props.user);
-    //   if (!user) {
-    //     props.history.push('/login');
-    //   }
-    // }, [])
+function VideoPage(props) {
 
-    class VideoPlayer extends React.Component {
+  // useEffect(() => {
+  //   const user = checkUserCreds(props.user);
+  //   if (!user) {
+  //     props.history.push('/login');
+  //   }
+  // }, [])
 
-    
-      // console.log(props)
-    
-        componentDidMount(props) {
-          this.player = videojs(this.videoNode, this.props)
-        }
-      
-        componentWillUnmount() {
-          if (this.player) {
-            this.player.dispose()
-          }
-        }
-    
-        render() {
-          let {windowHeight, windowWidth} = this.props
-          let newWidth = windowWidth *.60
-          let newHeight = newWidth * .5625
-          // console.log("rendering:", this)
-    
-          return (
-            <div className="video-player">
-    
-              <div  data-vjs-player style={{
-                width: newWidth,
-                height: newHeight
-              }}>
-                <video ref={(node) => { this.videoNode = node; }} className="video-js" />
-              </div>
-            
-              </div>
-          );
-        }
+  class VideoPlayer extends React.Component {
+
+
+    // console.log(props)
+
+    componentDidMount(props) {
+      this.player = videojs(this.videoNode, this.props)
+    }
+
+    componentWillUnmount() {
+      if (this.player) {
+        this.player.dispose()
       }
+    }
 
-    let [stateWidth, setWidth] = useState(window.innerWidth)
-    let [stateHeight, setHeight] = useState(window.innerHeight)
+    render() {
+      let { windowHeight, windowWidth } = this.props
+      let newWidth = windowWidth * .60
+      let newHeight = newWidth * .5625
+      // console.log("rendering:", this)
 
+      return (
+        <div className="video-player">
 
+          <div data-vjs-player style={{
+            width: newWidth,
+            height: newHeight
+          }}>
+            <video ref={(node) => { this.videoNode = node; }} className="video-js" />
+          </div>
 
-    return (
-              <div className="grid-container">
-            {/* <img className="img-fresh-logo" src={freshLogo}/> */}
-            
-            <h2 className="registration-heading-grid">under one sky</h2>
-
-            <div className="video-row" style={{padding: "3vh"}}>
-              {/* <div className="chat-area">Test</div> */}
-              <img style={{width:"100%"}} src="https://i.imgur.com/xynxGRR.png"></img>
-            
-            </div>
-              {/* <FakeChat windowHeight={stateHeight} windowWidth={stateWidth}/> */}
-            <img className="grid-heading" style={{width: "6vw"}} src={freshLogo}/>
-            {/* <ClientPendingBanner subject="event"/> */}
-            {/* <div className="heads-up">This is a staging page for testing purposes only.</div> */}
         </div>
-
-
-    );
+      );
+    }
   }
-  
-  
-  export default VideoPage
+
+  let [stateWidth, setWidth] = useState(window.innerWidth)
+  let [stateHeight, setHeight] = useState(window.innerHeight)
+
+
+
+  return (
+    <div className="grid-container">
+      {/* <img className="img-fresh-logo" src={freshLogo}/> */}
+
+      <h2 className="registration-heading-grid">under one sky</h2>
+
+      <div className="video-row" style={{ padding: "3vh" }}>
+        {/* <div className="chat-area">Test</div> */}
+        <img style={{ width: "100%" }} src="https://i.imgur.com/xynxGRR.png"></img>
+
+      </div>
+      {/* <FakeChat windowHeight={stateHeight} windowWidth={stateWidth}/> */}
+      <img className="grid-heading" style={{ width: "6vw" }} src={freshLogo} />
+      {/* <ClientPendingBanner subject="event"/> */}
+      {/* <div className="heads-up">This is a staging page for testing purposes only.</div> */}
+    </div>
+
+
+  );
+}
+
+
+export default VideoPage
