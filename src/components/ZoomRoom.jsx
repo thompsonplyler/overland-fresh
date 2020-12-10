d671efe69037a5001e051b1cb1b2a6c6e3be46ba
thompsonplyler <nealok.g@gmail.com>
2020-12-10 10:25:51 -0500

    Solved weird 'ZoomRoom.jsx' error.

--- src/components/ZoomRoom.jsx
+++ src/components/ZoomRoom.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import '../App.css';
-import { ZoomMtg } from '@zoomus/websdk'
+// import React from 'react'
+// import '../App.css';
+// import { ZoomMtg } from '@zoomus/websdk'
 
-ZoomMtg.setZoomJSLib('node_modules/@zoomus/websdk/dist/lib', '/av');
-ZoomMtg.preLoadWasm();
-ZoomMtg.prepareJssdk
+// ZoomMtg.setZoomJSLib('node_modules/@zoomus/websdk/dist/lib', '/av');
+// ZoomMtg.preLoadWasm();
+// ZoomMtg.prepareJssdk
 
 
-function ZoomRoom() {
-    return (
-        <div>
-            Test
-        </div>
-    )
-}
+// function ZoomRoom() {
+//     return (
+//         <div>
+//             Test
+//         </div>
+//     )
+// }
 
-export default ZoomRoom;
+// export default ZoomRoom;
