4335671e5947a7aecc78d1994f495efccc22fbf6
Thompson Plyler <nealok.g@gmail.com>
2021-01-19 13:14:13 -0500

    Changed user credential check to clear existing users.

--- src/components/checkUserCreds/index.js
+++ src/components/checkUserCreds/index.js
@@ -1,6 +1,44 @@
 export const checkUserCreds = (user) => {
-  console.log("LocalStorage from checkUserCreds: ",localStorage)
-  console.log("User information sent to checkUserCreds: ", user)
+
+  if (localStorage.length > 0) {
+    console.log(`
+    
+    Local storage is not empty.
+    
+    `)
+    console.log(`
+
+    Checking to see if the user has been cleared to return for 2021...
+    
+    `)
+    if (window.localStorage.getItem('cleared')=='true'){
+      console.log(`
+      
+      User has already had local storage cleared in order to access site.`)
+      
+    }
+    else {
+
+      console.log(`
+      
+      No clear flag found...
+      
+      `)
+      console.log(`
+      
+      Clearing local storage...
+      
+      `)
+      localStorage.clear()
+
+      console.log(`
+      
+      Setting cleared flag to TRUE
+
+      `)
+      localStorage.setItem('cleared','true')
+    }
+  }
 
     let loggedIn = false; 
     if (user.email) {
