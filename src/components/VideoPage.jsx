2dce170f2acb8d248a7c7570221553bc858651ca
Thompson Plyler <nealok.g@gmail.com>
2021-01-21 09:13:00 -0500

    Changed URL to ensure China media url

--- src/components/VideoPage.jsx
+++ src/components/VideoPage.jsx
@@ -25,17 +25,17 @@ import HLSSource from './HLSSource';
 
 function VideoPage(props) {
   // actual normal feed
-  const normalStreamURL = "https://b1ec00ae2bfa.us-east-1.playback.live-video.net/api/video/v1/us-east-1.023900886900.channel.rXLMiU83NvaX.m3u8"
+  // const normalStreamURL = "https://b1ec00ae2bfa.us-east-1.playback.live-video.net/api/video/v1/us-east-1.023900886900.channel.rXLMiU83NvaX.m3u8"
   // actual China feed with base64 hash as stream key
   const chinaURL = "http://fresh-play.ccsupport.cn/live/67a4c84cb83788005285d9c9e6f6d6c046b4c39e/playlist.m3u8"
   // NS feed for testing
-  const usURL = "https://b1ec00ae2bfa.us-east-1.playback.live-video.net/api/video/v1/us-east-1.023900886900.channel.k2VuaaM6o9yb.m3u8"
+  // const usURL = "https://b1ec00ae2bfa.us-east-1.playback.live-video.net/api/video/v1/us-east-1.023900886900.channel.k2VuaaM6o9yb.m3u8"
   const [chatButtonPressed, setChatButtonPressed] = useState(false)
   let [stateWidth, setWidth] = useState(window.innerWidth)
   let [firstName, setFirstName] = useState("Unknown")
   let [lastName,setLastName] = useState("User")
   let [location,setLocation] = useState(["elsewhere"])
-  let [streamURL, setStreamURL] = useState(normalStreamURL)
+  // let [streamURL, setStreamURL] = useState(normalStreamURL)
   let [isChina,setChina] = useState(false)
   let [isUS, setUS] = useState(false)
 
@@ -84,22 +84,22 @@ function VideoPage(props) {
 
   },[])
 
-useEffect(()=>{
-    fetch('https://extreme-ip-lookup.com/json/')
-.then( res => res.json())
-.then(response => {
-    console.log("Country: ", response.country);
-    if (response.country == "China"){
-      setChina(true)
-    }
-    if (response.country == "United States"){
-      setUS(true)
-    }
- })
- .catch((data, status) => {
-    console.log('Request failed');
- })
-},[])
+// useEffect(()=>{
+//     fetch('https://extreme-ip-lookup.com/json/')
+// .then( res => res.json())
+// .then(response => {
+//     console.log("Country: ", response.country);
+//     if (response.country == "China"){
+//       setChina(true)
+//     }
+//     if (response.country == "United States"){
+//       setUS(true)
+//     }
+//  })
+//  .catch((data, status) => {
+//     console.log('Request failed');
+//  })
+// },[])
 
 console.log(location)
 
@@ -116,7 +116,7 @@ console.log(location)
       <Player fluid={false} width={stateWidth*.55} playsInline poster={poster} autoplay={true}>
       <HLSSource
       isVideoChild
-      src={isChina?chinaURL:normalStreamURL}
+      src={chinaURL}
       />
       <BigPlayButton position="center"></BigPlayButton>
     </Player>
