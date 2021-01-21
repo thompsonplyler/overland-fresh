c99db3904dd1f70278dc3325d72ac9bbbd209375
Thompson Plyler <nealok.g@gmail.com>
2021-01-21 06:09:54 -0500

    Added code to check IP.

--- src/components/VideoPage.jsx
+++ src/components/VideoPage.jsx
@@ -73,6 +73,15 @@ function VideoPage(props) {
 
   },[])
 
+  fetch('https://extreme-ip-lookup.com/json/')
+.then( res => res.json())
+.then(response => {
+    console.log("Country: ", response.country);
+ })
+ .catch((data, status) => {
+    console.log('Request failed');
+ })
+
 
   return (
     <div>
