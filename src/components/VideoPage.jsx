c5133b4d64ff290ebb0543461c6e7ad07abf576a
Thompson Plyler <nealok.g@gmail.com>
2021-01-21 19:59:26 -0500

    prepping for release to final

--- src/components/VideoPage.jsx
+++ src/components/VideoPage.jsx
@@ -19,31 +19,24 @@ import { AGENDA_URL, LOGIN_URL } from '../urls';
 // import Dat from './Dat'
 import Iframe from 'react-iframe'
 
-
-
-
-
-const videoJsOptions = {
-  autoplay: true,
-  controls: true,
-  loop: true,
-  responsive: true,
-  mute: true,
-  poster: "https://i.imgur.com/Aaog0bm.png",
-  sources: [{
-    src: tempVideo,
-    // src: "https://b1ec00ae2bfa.us-east-1.playback.live-video.net/api/video/v1/us-east-1.023900886900.channel.rXLMiU83NvaX.m3u8",
-    poster: "https://i.imgur.com/Aaog0bm.png"
-  }]
-}
-
-
-
-
-
-
 function VideoPage(props) {
+  
+  // actual normal feed
+  const normalStreamURL = "https://b1ec00ae2bfa.us-east-1.playback.live-video.net/api/video/v1/us-east-1.023900886900.channel.rXLMiU83NvaX.m3u8"
+  // actual China feed with base64 hash as stream key
+  const chinaURL = "http://fresh-play.ccsupport.cn/live/67a4c84cb83788005285d9c9e6f6d6c046b4c39e/playlist.m3u8"
+  // NS feed for testing
+  const usURL = "https://b1ec00ae2bfa.us-east-1.playback.live-video.net/api/video/v1/us-east-1.023900886900.channel.k2VuaaM6o9yb.m3u8"
   const [chatButtonPressed, setChatButtonPressed] = useState(false)
+  let [stateWidth, setWidth] = useState(window.innerWidth)
+  let [firstName, setFirstName] = useState("Unknown")
+  let [lastName,setLastName] = useState("User")
+  let [location,setLocation] = useState(["elsewhere"])
+  let [streamURL, setStreamURL] = useState(normalStreamURL)
+  let [isChina,setChina] = useState(false)
+  let [isUS, setUS] = useState(false)
+
+  console.log("Video Page props: ",props.user)
 
   useEffect(() => {
     const checkLogin = async () => {
@@ -62,71 +55,57 @@ function VideoPage(props) {
   , [])
 
   useEffect(()=>{
-    window.addEventListener('onfullscreenchange',()=>{
+
+    function handleResize(){
       console.log("I've been resized!")
-      console.log(window)
-    })
-  })
-
-  const chatToggle = (e) => {
-    e.preventDefault()
-    setChatButtonPressed(!chatButtonPressed)
-
-  }
-
-
-  class VideoPlayer extends React.Component {
-
-
-    // console.log(props)
-
-    componentDidMount(props) {
-      this.player = videojs(this.videoNode, this.props)
+      setWidth(window.innerWidth)
     }
-
-    componentWillUnmount() {
-      if (this.player) {
-        this.player.dispose()
-      }
-    }
-
-
-    handleFullScreen =(thing)=>{
-      console.log("Finding thing",thing)
-      }
-    
-    render() {
-
-      let { windowHeight, windowWidth } = this.props
-      let newWidth = windowWidth * .55
-      // let newWidth = windowWidth * .40
-      let newHeight = newWidth * .5625
-
-
-      // console.log("rendering:", this)
       
-      return (
-        <div className="video-player">
-          
-          
-          <div data-vjs-player style={{
-            width: newWidth,
-            height: newHeight
-          }}>
-            <video ref={(node) => { this.videoNode = node; }} className="video-js" />
-          </div>
-
-        </div>
-      );
+      
+    window.addEventListener('resize',handleResize)
+  
+  
+    })
+  
+    //activate and deactivate chat 
+    const chatToggle = (e) => {
+      e.preventDefault()
+      setChatButtonPressed(!chatButtonPressed)
+  
     }
-  }
+  
+    useEffect(()=>{
+      console.log("User props: ",props.user)
+      if (props.user.firstname) setFirstName(props.user.firstname)
+      if (props.user.lastname) setLastName(props.user.lastname)
+      if (localStorage.user) {
+        let parsed = JSON.parse(localStorage.user)
+        console.log("Parsed stuff: ",parsed)
+        setFirstName(parsed.firstname)
+        setLastName(parsed.lastname)
+      }
+  
+    },[])
+  
+  useEffect(()=>{
+      fetch('https://extreme-ip-lookup.com/json/')
+  .then( res => res.json())
+  .then(response => {
+      console.log("Country: ", response.country);
+      if (response.country == "China"){
+        setChina(true)
+      }
+      if (response.country == "United States"){
+        setUS(true)
+      }
+   })
+   .catch((data, status) => {
+      console.log('Request failed');
+   })
+  },[])
 
-  let [stateWidth, setWidth] = useState(window.innerWidth)
-  let [stateHeight, setHeight] = useState(window.innerHeight)
-
-
-  let firstName = JSON.parse(localStorage.getItem('user')).firstname
-  let lastName = JSON.parse(localStorage.getItem('user')).lastname
+  firstName = JSON.parse(localStorage.getItem('user')).firstname
+  lastName = JSON.parse(localStorage.getItem('user')).lastname
   return (
     <div>
     <div className="flex-container-video">
@@ -134,8 +113,13 @@ function VideoPage(props) {
       <h2 className="event-heading-1">under one sky</h2>  
       <h3 className="event-video-title">Part 2: Under One Sky Meeting</h3>
       <div className="video-row">
-        {/* <div className="chat-area">Test</div> */}
-        <VideoPlayer windowHeight={stateHeight} windowWidth={stateWidth}{...videoJsOptions} />
+      <Player fluid={false} width={stateWidth*.55} playsInline poster={poster} autoplay={true}>
+      <HLSSource
+      isVideoChild
+      src={isChina?chinaURL:normalStreamURL}
+      />
+      <BigPlayButton position="center"></BigPlayButton>
+    </Player>
         <div className="button-video-return">
           <Link to={AGENDA_URL}><button style={{width: "200px"}}>Return to Main Page</button></Link>
         </div>
