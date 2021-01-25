9f051bd49ed49d9a630c2b02c7d8865f3de2eeb2
Thompson Plyler <nealok.g@gmail.com>
2021-01-25 12:06:58 -0500

    Changed testing URLs for Liminal

--- src/components/VideoPageSawdust.jsx
+++ src/components/VideoPageSawdust.jsx
@@ -84,7 +84,7 @@ function VideoPageSawdust(props) {
       <Player fluid={false} width={stateWidth*.55} playsInline poster={poster} autoplay={true}>
       <HLSSource
       isVideoChild
-      src={CHINA_NS_VIDEO}
+      src="http://liminal-push.ccsupport.cn/live/live/UMkMlXOwRgJSzPgXxQyw1w/playlist.m3u8"
       />
       <BigPlayButton position="center"></BigPlayButton>
     </Player>
