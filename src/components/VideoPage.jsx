bb534cb1233af02e475972d07549878c47d7696f
Thompson Plyler <nealok.g@gmail.com>
2021-01-26 03:53:30 -0500

    Chat popout works for the most part. Not beautiful, but whaddareyagonnado?

--- src/components/VideoPage.jsx
+++ src/components/VideoPage.jsx
@@ -18,30 +18,23 @@ import HLSSource from './HLSSource';
 import NewWindow from 'react-new-window'
 
 function VideoPage(props) {
-  
-  // actual normal feed
-  const normalStreamURL = "https://b1ec00ae2bfa.us-east-1.playback.live-video.net/api/video/v1/us-east-1.023900886900.channel.rXLMiU83NvaX.m3u8"
-  // actual China feed with base64 hash as stream key
-  const chinaURL = "http://fresh-play.ccsupport.cn/live/67a4c84cb83788005285d9c9e6f6d6c046b4c39e/playlist.m3u8"
-  // NS feed for testing
-  const usURL = "https://b1ec00ae2bfa.us-east-1.playback.live-video.net/api/video/v1/us-east-1.023900886900.channel.k2VuaaM6o9yb.m3u8"
+
   const [chatButtonPressed, setChatButtonPressed] = useState(false)
+  const [popOut,setPopOut] = useState(false)
   let [stateWidth, setWidth] = useState(window.innerWidth)
   let [firstName, setFirstName] = useState("Unknown")
   let [lastName,setLastName] = useState("User")
-  let [location,setLocation] = useState(["elsewhere"])
-  let [streamURL, setStreamURL] = useState(normalStreamURL)
-  let [isChina,setChina] = useState(false)
-  let [isUS, setUS] = useState(false)
+  
+  
 
-  // console.log("Video Page props: ",props.user)
 
+// must re-add this when bringing code into final
   useEffect(() => {
     const checkLogin = async () => {
     document.body.classList.remove('sawdust-body')
-    // console.log("Props from Login: ", props)
+    
     let user = await checkUserCreds(props.user);
-    // console.log("User result from checkUserCreds: ",user)
+    
     if (user == false) {
   
       props.history.push(LOGIN_URL);
@@ -50,18 +43,6 @@ function VideoPage(props) {
   checkLogin()
   }, [])
 
-  useEffect(()=>{
-
-    function handleResize(){
-      console.log("I've been resized!")
-      setWidth(window.innerWidth)
-    }
-      
-      
-    window.addEventListener('resize',handleResize)
-  
-  
-    })
 useEffect(()=>{
   if (document.getElementsByClassName("ps__rail-x")){
     let kill = document.getElementsByClassName("ps__rail-x")
@@ -80,7 +61,6 @@ useEffect(()=>{
     const chatToggle = (e) => {
       e.preventDefault()
       setChatButtonPressed(!chatButtonPressed)
-  
     }
   
     useEffect(()=>{
@@ -95,6 +75,28 @@ useEffect(()=>{
       }
   
     },[])
+
+    const popOutToggle = () =>{
+      setPopOut(!popOut)
+      setChatButtonPressed(!chatButtonPressed)
+    }
+
+    const popOutUnloader = () => {
+      console.log("You have unloaded the popout chat!")
+    }
+
+    const popItOut = () => {
+      return await(<div><NewWindow copystyles="true" 
+      name="popout-chat" 
+      title="fresh - Under One Sky 2021 Chat">
+        <Iframe url={`https://www.deadsimplechat.com/CHsOaJ9WD?username=${firstName}%20${lastName}`}
+        width="100%"
+        height="100%"
+  className="chat-box" 
+  display="flex" id="newWindowId"
+  />
+  </NewWindow></div>)
+    }
   
 
   firstName = JSON.parse(localStorage.getItem('user')).firstname
@@ -106,19 +108,13 @@ useEffect(()=>{
       <h2 className="event-heading-1">under one sky</h2>  
       <h3 className="event-video-title">Part 2: Under One Sky Meeting</h3>
       <div className="video-row">
-        <div style={{width:"100px"}}>
-        <NewWindow copystyles="true" 
-        name="popout-chat" 
-        title="fresh - Under One Sky 2021 Chat" 
-        features={{width:"10px"}}>
-          <Iframe url={`https://www.deadsimplechat.com/CHsOaJ9WD?username=${firstName}%20${lastName}`}
-    className="chat-box"
-    /></NewWindow>
-    </div>
+        {/* <div style={{width:"100px"}}> */}
+    {popOut?popItOut():null}
+    {/* </div> */}
       <Player fluid={false} width={stateWidth*.55} playsInline poster={poster} autoplay={true}>
       <HLSSource
       isVideoChild
-      src={isChina?chinaURL:normalStreamURL}
+      src="https://b1ec00ae2bfa.us-east-1.playback.live-video.net/api/video/v1/us-east-1.023900886900.channel.rXLMiU83NvaX.m3u8"
       />
       <BigPlayButton position="center"></BigPlayButton>
     </Player>
@@ -129,6 +125,7 @@ useEffect(()=>{
       
         <img className="grid-heading" style={{ width: "6vw" }} src={freshLogo} />
     </div>
+    <img onClick={popOutToggle} className={chatButtonPressed?'popout-icon-active':'popout-icon-inactive'} src='./popout_icon.png'></img>
     <img onClick={chatToggle} className={chatButtonPressed?'chat-icon-active':'chat-icon-inactive'} src='./chat_icon.png'></img>
     {chatButtonPressed?<Iframe url={`https://www.deadsimplechat.com/CHsOaJ9WD?username=${firstName}%20${lastName}`}
     width="21%"
@@ -147,9 +144,6 @@ useEffect(()=>{
     }
 
     </div>
-
-
-
   );
 }
 
